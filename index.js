1f1b44e88c8f4e522cd9baf59da4890b1a06f65d
Eesa Zahed <eesa.zahed@gmail.com>
2023-11-25 21:23:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ dotenv.config();
 const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 app.use(express.json());
 
 // Socket
